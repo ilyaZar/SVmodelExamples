57bb5b5fa26ff829abfedd13a5c74d1a3a311d88
IlyaZ <zarubin@wiso.uni-koeln.de>
2021-08-22 21:39:37 +0200

    update example run

--- 03_example.R
+++ 03_example.R
@@ -2,7 +2,7 @@ library(SVmodelRcppSMC)
 # setting the seed for reproducibility
 set.seed(123)
 # setting model parameters
-Tinit      <- 100
+Tinit      <- 1000
 phiXinit   <- 0.9
 sigmaXinit <- 0.2
 betaYinit  <- 0.7
@@ -17,21 +17,30 @@ xt <- data_simul_sv$statesXt
 yt <- data_simul_sv$measurementsYt
 
 # set the number of particles
-particleNumber <- 20
+particleNumber <- 10
 MM    <- 5000
 burn  <- 4000 #round(MM/2)
 
 startingVals      <- c(phiXinit, 20, 20)
-conditionalXinit <- c(Xinit, xt)
+conditionalXinit  <- c(Xinit, xt) + rnorm(Tinit + 1, sd = 3)
 
-system.time(
-  outPG <- svModelPGr(y = yt,
-                      numParticles = particleNumber,
-                      startingValues = startingVals,
-                      numIter = MM,
-                      xRinit = conditionalXinit,
-                      10)
-)
+outPG <- svModelPGr(y = yt,
+                    numParticles = particleNumber,
+                    startingValues = startingVals,
+                    numIter = MM,
+                    xRinit = conditionalXinit,
+                    10)
 plotPMCMCoutput(outputPMCMC =  outPG,
+                burnin = burn,
+                trueVals = c(sigmaXinit, betaYinit))
+
+particleNumber <- 100
+outPG2 <- svModelPGr(y = yt,
+                     numParticles = particleNumber,
+                     startingValues = startingVals,
+                     numIter = MM,
+                     xRinit = conditionalXinit,
+                     10)
+plotPMCMCoutput(outputPMCMC =  outPG2,
                 burnin = burn,
                 trueVals = c(sigmaXinit, betaYinit))
